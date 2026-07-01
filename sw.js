efafc184d0372da40e742d340a5f8c7c596e9ce1
alexandermarquez0pp-jpg <alexandermarquez0pp@gmail.com>
2026-06-30 23:27:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'prodtracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './js/auth.js',
+  './js/store.js',
+  './js/reports.js',
+  './js/firebase-config.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Fallback to network if not in cache
+        return response || fetch(event.request);
+      })
+  );
+});
